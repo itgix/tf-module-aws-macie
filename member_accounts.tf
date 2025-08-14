cd7af259272b360a67810d8b72e71bb325538407
Boris Yakimov <boris.yakimov@itgix.com>
2025-08-14 11:22:20 +0300

    #202 move delegated admin to org

--- member_accounts.tf
+++ member_accounts.tf
@@ -1,8 +1,3 @@
-resource "aws_macie2_organization_admin_account" "delegate_admin_sec_acc" {
-  count            = var.macie_organization_management_account ? 1 : 0
-  admin_account_id = var.organization_security_account_id
-}
-
 resource "aws_macie2_member" "members" {
   count                                 = var.macie_organization_security_account ? length(var.organization_member_account_ids) : 0
   account_id                            = var.organization_member_account_ids[count.index]
